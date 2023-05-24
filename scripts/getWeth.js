afb1c324e978d674097c4508ed87a82d3e2e1445
Luke Sheppard <sheppiegit@gmail.com>
2023-05-24 15:46:19 -0400

    initial getWeth implementation with mainnet hardcoded addresses etc.

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -1,4 +1,6 @@
-const { getNamedAccounts } = require('hardhat');
+const { getNamedAccounts, ethers } = require('hardhat');
+
+const AMOUNT = ethers.utils.parseEther('0.02');
 
 const getWeth = async () => {
   const { deployer } = await getNamedAccounts();
@@ -6,6 +8,18 @@ const getWeth = async () => {
   //call the 'deposit' function on the weth contract
   //need contract address and abi
   //0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+
+  const iWeth = await ethers.getContract(
+    'IWETH',
+    '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    deployer
+  );
+
+  const tx = await iWeth.deposit({ value: AMOUNT });
+  await tx.wait(1);
+
+  const wethBalance = await iWeth.balanceOf(deployer);
+  console.log(`Got ${wethBalance.toString()} WETH`);
 };
 
 module.exports = getWeth;
