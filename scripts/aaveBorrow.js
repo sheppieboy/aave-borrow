b5920f9c492dc9148f2338a0046d2ea89affaeb0
Luke Sheppard <sheppiegit@gmail.com>
2023-05-25 17:05:56 -0400

    Added an approve function and uses the lending pool contract to depoist the weth.

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,5 +1,5 @@
 const { getNamedAccounts, ethers } = require('hardhat');
-const { getWeth } = require('./getWeth');
+const { getWeth, AMOUNT } = require('./getWeth');
 
 const main = async () => {
   //aave treats everything like ERC-20 token
@@ -9,7 +9,19 @@ const main = async () => {
   //abi, address to interact with aave protocol
   const lendingPool = await getLendingPool(deployer);
   console.log(lendingPool.address);
+
+  //token to deposit
+  const wethTokenAddress = '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5';
+
+  //approve
+  await approveERC20(wethTokenAddress, lendingPool.address, AMOUNT, deployer);
+  console.log(`Depositing...`);
+
+  //deposit
+  await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
+  console.log('Deposited!');
 };
+
 const getLendingPool = async (account) => {
   const lendingPoolAddressProvider = await ethers.getContractAt(
     'ILendingPoolAddressesProvider',
@@ -24,6 +36,22 @@ const getLendingPool = async (account) => {
     account
   );
   return lendingPool;
+};
+
+const approveERC20 = async (
+  erc20Address,
+  spenderAddress,
+  amountToSpend,
+  account
+) => {
+  const erc20Token = await ethers.getContractAt(
+    'IERC20',
+    erc20Address,
+    account
+  );
+  const tx = await erc20Token.approve(spenderAddress, amountToSpend);
+  await tx.wait(1);
+  console.log('Approved!');
 };
 
 main()
