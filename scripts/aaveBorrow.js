b06a04aa2365d7ccdb189883beb86989fc38865c
Luke Sheppard <sheppiegit@gmail.com>
2023-05-25 16:49:04 -0400

    Added the getLending pool async function that uses the Lending pool addresses provider and lending pool interface.

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,8 +1,28 @@
+const { getNamedAccounts, ethers } = require('hardhat');
 const { getWeth } = require('./getWeth');
 
 const main = async () => {
   //aave treats everything like ERC-20 token
   await getWeth();
+
+  const { deployer } = await getNamedAccounts();
+  //abi, address to interact with aave protocol
+
+  //Lending Pool Address Provider
+};
+const getLendingPool = async (account) => {
+  const lendingPoolAddressProvider = await ethers.getContractAt(
+    'ILendingPoolAddressesProvider',
+    '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
+    account
+  );
+
+  const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool();
+  const lendingPool = await ethers.getContractAt(
+    'ILendingPool',
+    lendingPoolAddress,
+    account
+  );
 };
 
 main()
